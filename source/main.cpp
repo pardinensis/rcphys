9b629a45dcd15e98679bdab8cbd0cd7163f4f6c0
Christian Lobmeier <christian.lobmeier@esolutions.de>
2019-02-28 19:39:39 +0100

    asdkj

--- source/main.cpp
+++ source/main.cpp
@@ -3,36 +3,48 @@
 
 
 int main() {
-	std::vector<float> verticalAccels;
-	for (int i = 0; i < 8; ++i) {
-		verticalAccels.push_back(30);
-	}
-	verticalAccels.push_back(20);
-	verticalAccels.push_back(10);
-	for (int i = 0; i < 30; ++i) {
-		verticalAccels.push_back(0);
-	}
-	verticalAccels.push_back(20);
-	verticalAccels.push_back(10);
-	for (int i = 0; i < 8; ++i) {
-		verticalAccels.push_back(30);
-	}
+    std::vector<float> verticalAccels;
+    std::vector<float> rotationalAccels;
+    for (int i = 0; i < 8; ++i) {
+        verticalAccels.push_back(30);
+        rotationalAccels.push_back(0);
+    }
+    verticalAccels.push_back(20);
+    rotationalAccels.push_back(0);
+    verticalAccels.push_back(10);
+    rotationalAccels.push_back(0);
+    for (int i = 0; i < 15; ++i) {
+        verticalAccels.push_back(0);
+        rotationalAccels.push_back(2.79);
+    }
+    for (int i = 0; i < 15; ++i) {
+        verticalAccels.push_back(0);
+        rotationalAccels.push_back(-2.79);
+    }
+    verticalAccels.push_back(20);
+    rotationalAccels.push_back(0);
+    verticalAccels.push_back(10);
+    rotationalAccels.push_back(0);
+    for (int i = 0; i < 8; ++i) {
+        verticalAccels.push_back(30);
+        rotationalAccels.push_back(0);
+    }
 
 
     TrackPoint tp;
     tp.linearVelocity = 30;
-    tp.rotationalVelocity = 1.2;
+    tp.rotationalVelocity = 0;
 
-	std::cout << "x: " << glm::to_string(tp.getRight()) << std::endl;
-	std::cout << "y: " << glm::to_string(tp.getUp()) << std::endl;
-	std::cout << "z: " << glm::to_string(tp.getBackward()) << std::endl;
+    std::cout << "x: " << glm::to_string(tp.getRight()) << std::endl;
+    std::cout << "y: " << glm::to_string(tp.getUp()) << std::endl;
+    std::cout << "z: " << glm::to_string(tp.getBackward()) << std::endl;
 
     std::ofstream ofs("airtime.txt");
-    for (float a : verticalAccels) {
-    	ofs << tp.pos.x << " " << -tp.pos.z << " " << tp.pos.y << " ";
-    	glm::vec3 trackpos = tp.getTrackPos();
-    	ofs << trackpos.x << " " << -trackpos.z << " " << trackpos.y << std::endl;
-    	tp = tp.next(a, 0, 0);
+    for (int i = 0; i < verticalAccels.size(); ++i) {
+        ofs << tp.pos.x << " " << -tp.pos.z << " " << tp.pos.y << " ";
+        glm::vec3 trackpos = tp.getTrackPos();
+        ofs << trackpos.x << " " << -trackpos.z << " " << trackpos.y << std::endl;
+        tp = tp.next(verticalAccels[i], 0, rotationalAccels[i]);
     }
 
     return 0;
