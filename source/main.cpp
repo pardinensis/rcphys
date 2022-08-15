2eb8a901d5ae46d21ec198cd7616c7e34b775321
Christian Lobmeier <pardinensis@gmail.com>
2022-08-15 18:48:22 +0200

    zlib

--- source/main.cpp
+++ source/main.cpp
@@ -49,5 +49,27 @@ int main() {
 
     exportTrackSegment(track);
 
+
+
+    char a[50] = "Hello Hello Hello Hello Hello Hello!";
+    char b[50];
+
+    z_stream defstream;
+    defstream.zalloc = Z_NULL;
+    defstream.zfree = Z_NULL;
+    defstream.opaque = Z_NULL;
+
+    defstream.avail_in = (uInt)strlen(a)+1; // size of input, string + terminator
+    defstream.next_in = (Bytef *)a; // input char array
+    defstream.avail_out = (uInt)sizeof(b); // size of output
+    defstream.next_out = (Bytef *)b; // output char array
+
+    deflateInit(&defstream, Z_BEST_COMPRESSION);
+    deflate(&defstream, Z_FINISH);
+    deflateEnd(&defstream);
+
+    printf("Compressed size is: %lu\n", strlen(b));
+    printf("Compressed string is: %s\n", b);
+
     return 0;
 }
