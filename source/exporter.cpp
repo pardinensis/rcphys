6cadc800a2277249de6df78a774c1bb0f1376260
Christian Lobmeier <pardinensis@gmail.com>
2022-08-15 18:48:22 +0200

    b&m track

--- source/exporter.cpp
+++ source/exporter.cpp
@@ -1,30 +1,100 @@
 #include "exporter.hpp"
 #include "utils.hpp"
 
-const std::vector<glm::vec3> vertices = {
-    {-0.6f, -1.4f, 0.0f}, {-0.6f, -1.6f, 0.0f}, {-0.8f, -1.6f, 0.0f}, {-0.8f, -1.4f, 0.0f},
-    {0.8f, -1.4f, 0.0f},  {0.8f, -1.6f, 0.0f},  {0.6f, -1.6f, 0.0f},  {0.6f, -1.4f, 0.0f},
+const std::vector<glm::vec3> spineVertices = {
+    // left spine
+    {-0.8f + 0.1414f, -1.5f + 0.0f, 0.0f},
+    {-0.8f + 0.1f, -1.5f + 0.1f, 0.0f},
+    {-0.8f + 0.0f, -1.5f + 0.1414f, 0.0f},
+    {-0.8f - 0.1f, -1.5f + 0.1f, 0.0f},
+    {-0.8f - 0.1414f, -1.5f + 0.0f, 0.0f},
+    {-0.8f - 0.1f, -1.5f - 0.1f, 0.0f},
+    {-0.8f + 0.0f, -1.5f - 0.1414f, 0.0f},
+    {-0.8f + 0.1f, -1.5f - 0.1f, 0.0f},
+
+    // right spine
+    {0.8f + 0.1414f, -1.5f + 0.0f, 0.0f},
+    {0.8f + 0.1f, -1.5f + 0.1f, 0.0f},
+    {0.8f + 0.0f, -1.5f + 0.1414f, 0.0f},
+    {0.8f - 0.1f, -1.5f + 0.1f, 0.0f},
+    {0.8f - 0.1414f, -1.5f + 0.0f, 0.0f},
+    {0.8f - 0.1f, -1.5f - 0.1f, 0.0f},
+    {0.8f + 0.0f, -1.5f - 0.1414f, 0.0f},
+    {0.8f + 0.1f, -1.5f - 0.1f, 0.0f},
+
+    // center spine
+    {0.0f + 0.3f, -2.0f + 0.3f, 0.0f},
+    {0.0f - 0.3f, -2.0f + 0.3f, 0.0f},
+    {0.0f - 0.3f, -2.0f - 0.3f, 0.0f},
+    {0.0f + 0.3f, -2.0f - 0.3f, 0.0f},
 };
 
-const std::vector<glm::uvec3> triangles = {
-    {0, 1, 9},  {0, 9, 8},   {1, 2, 10}, {1, 10, 9},  {2, 3, 11}, {2, 11, 10}, {3, 0, 8},  {3, 8, 11},
-    {4, 5, 13}, {4, 13, 12}, {5, 6, 14}, {5, 14, 13}, {6, 7, 15}, {6, 15, 14}, {7, 4, 12}, {7, 12, 15},
+const std::vector<glm::uvec3> spineTriangles = {
+    // left spine
+    {0, 20, 1},
+    {1, 20, 21},
+    {1, 21, 2},
+    {2, 21, 22},
+    {2, 22, 3},
+    {3, 22, 23},
+    {3, 23, 4},
+    {4, 23, 24},
+    {4, 24, 5},
+    {5, 24, 25},
+    {5, 25, 6},
+    {6, 25, 26},
+    {6, 26, 7},
+    {7, 26, 27},
+    {7, 27, 0},
+    {0, 27, 20},
+
+    // right spine
+    {8, 28, 9},
+    {9, 28, 29},
+    {9, 29, 10},
+    {10, 29, 30},
+    {10, 30, 11},
+    {11, 30, 31},
+    {11, 31, 12},
+    {12, 31, 32},
+    {12, 32, 13},
+    {13, 32, 33},
+    {13, 33, 14},
+    {14, 33, 34},
+    {14, 34, 15},
+    {15, 34, 35},
+    {15, 35, 8},
+    {8, 35, 28},
+
+    // center spine
+    {16, 36, 17},
+    {17, 36, 37},
+    {17, 37, 18},
+    {18, 37, 38},
+    {18, 38, 19},
+    {19, 38, 39},
+    {19, 39, 16},
+    {16, 39, 36},
 };
 
 void exportTrackSegment(const std::vector<TrackPoint>& track) {
     std::vector<glm::vec3> segmentVertices;
     std::vector<glm::uvec3> segmentIndices;
+
+    // spines
     for (unsigned int i = 0; i < track.size(); ++i) {
-        for (glm::vec3 v : vertices) {
+        for (glm::vec3 v : spineVertices) {
             segmentVertices.push_back(track[i].rot * v + track[i].pos);
         }
     }
     for (unsigned int i = 0; i < track.size() - 1; ++i) {
-        unsigned int indexOffset = i * vertices.size();
-        for (glm::uvec3 t : triangles) {
+        unsigned int indexOffset = i * spineVertices.size();
+        for (glm::uvec3 t : spineTriangles) {
             segmentIndices.push_back(glm::uvec3(t.x + indexOffset, t.y + indexOffset, t.z + indexOffset));
         }
     }
+
+    //
 
     std::ofstream ofs("track.obj");
     if (ofs.is_open()) {
